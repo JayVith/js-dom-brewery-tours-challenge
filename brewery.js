05bf7d3bfdf5af1722da65c343d212ca400424d1
Jay Vithani <jvithani@hotmail.com>
2023-04-24 10:08:42 +0100

    plan for core criteria only

---
+++ brewery.js
@@ -0,0 +1,34 @@
+// ### Standard
+// ![Standard](images/standard.png)
+// - A user can enter a US state and view a list of breweries in that state
+
+" 1 - GET state data from brewery API"
+" 2 - Use INSOMNIA to GET and ARRANGE data to my local server"
+" 3 - Use FETCH command on arranged local data to transmit to live site"
+" 4 - Test to see how it is working with deifferent states etc"
+
+// 	- The list should only shows the types of breweries that offer brewery tours:
+// 		- Micro
+// 		- Regional
+// 		- Brewpub
+// 	- **Do not** show the other types of breweries
+
+" 5 - FILTER for solely Micro, Regional and Brewpub type"
+" 6 - Add EVENT LISTENER and/or IF loop to sort brewery types????"
+
+// - From the list of breweries, a user can view the following details about each brewery:
+// 	- Name
+// 	- Type of brewery
+// 	- Address
+// 	- Phone Number
+
+" 7 - FILTER for specific criteria above"
+
+// - From the list of breweries, a user can visit the website of a brewery
+
+" 8 - APPEND ANCHOR for brewery website maybe using FILTER for link???? "
+" 9 - If no button present APPEND BUTTON for site"
+
+// - From the 'filter by type of brewery' section, a user can filter by type of brewery.
+
+" 10 - See step 5 but FILTER again for ONLY Micro or ONLY Regional or ONLY Brewpub"
